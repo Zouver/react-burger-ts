2c771231aac28f11e6f8efc480508ea0e709fd13
Andrey Ardvenev <a.ardvenev@bi.zone>
2026-07-20 20:54:36 +0300

    feat(app-header): new component

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -11,24 +11,31 @@ export const AppHeader = (): React.JSX.Element => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
-        <div className={styles.menu_part_left}>
-          {/* Тут должны быть ссылки, а не например кнопки или абзацы */}
-          <a href="/" className={`${styles.link} ${styles.link_active}`}>
-            <BurgerIcon type="primary" />
-            <p className="text text_type_main-default ml-2">Конструктор</p>
-          </a>
-          <a href="/feed" className={`${styles.link} ml-10`}>
-            <ListIcon type="secondary" />
-            <p className="text text_type_main-default ml-2">Лента заказов</p>
-          </a>
-        </div>
+        <ul className={styles.menu_part_left}>
+          <li>
+            <a href="/" className={`${styles.link} ${styles.link_active}`}>
+              <BurgerIcon type="primary" />
+              <p className="text text_type_main-default ml-2">Конструктор</p>
+            </a>
+          </li>
+          <li>
+            <a href="/feed" className={`${styles.link} ml-10`}>
+              <ListIcon type="secondary" />
+              <p className="text text_type_main-default ml-2">Лента заказов</p>
+            </a>
+          </li>
+        </ul>
         <div className={styles.logo}>
           <Logo />
         </div>
-        <a href="/profile" className={`${styles.link} ${styles.link_position_last}`}>
-          <ProfileIcon type="secondary" />
-          <p className="text text_type_main-default ml-2">Личный кабинет</p>
-        </a>
+        <ul className={styles.menu_part_right}>
+          <li>
+            <a href="/profile" className={`${styles.link} ${styles.link_position_last}`}>
+              <ProfileIcon type="secondary" />
+              <p className="text text_type_main-default ml-2">Личный кабинет</p>
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
