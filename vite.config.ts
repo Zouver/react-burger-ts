5aa0348136de5c582d3eb93cffb89e83f6449fd5
andrey <arddvenev.andrey@gmail.com>
2026-09-13 19:10:45 +0300

    build(deploy): настроить GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     checker({
       typescript: true,
@@ -19,7 +20,6 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-  base: '',
   test: {
     exclude: ['**/e2e/**', '**/node_modules/**', '**/dist/**'],
     globals: true,
